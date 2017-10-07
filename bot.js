03a4ae63ba07bcbdad0597e33cc0f61f7f99f3b1
fli136 <fli136@gmail.com>
2017-10-07 12:08:51 -0700

    cleaned up code

--- bot.js
+++ bot.js
@@ -1,26 +1,33 @@
-var Discord = require("discord.js");
-var bot = new Discord.Client();
-var config = require('./config.json');
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require('./config.json');
 const fs = require("fs");
 
 bot.on("message", msg => {
 
-  if(!msg.content.startsWith(config.prefix)) return;
-  // Exit if any bot
+  //Exits if bot
   if(msg.author.bot) return;
 
-  if (msg.content.startsWith(config.prefix + "ping")) {
+  //Checks for prefix
+  if(!msg.content.startsWith(config.prefix)) return;
+
+  const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command == "ping") {
     msg.channel.sendMessage("pong!");
   }
 
-  else if (msg.content.startsWith(config.prefix + "foo")) {
-    msg.channel.sendMessage("bar!");
-  }
+  // else if (msg.content.startsWith(config.prefix + "getLogs ")) {
+  //   let args = msg.content.split(" ");
+  //   let user = msg.mentions.users.first();
+  //   let messages = user.getLogs(3, be)
+  // }
 
-  else if (msg.content.startsWith(config.prefix + "change ")) {
-    let args = msg.content.split(" ");
+
+  if (command == "change") {
     let user = msg.mentions.users.first();
-    let newName = args[2];
+    let newName = args[1];
     msg.guild.member(user).setNickname(newName);
   }
 });
